baa39d6abe2d5206c27070136d1e03c6e5eca114
Gabriel Martins <martins.gabriel@aluno.ifsp.edu.br>
2020-09-13 20:33:23 -0300

    commit inicial

---
+++ pyxis/pyxis-java/src/main/java/com/martinsgms/pyxis/PyxisApplication.java
@@ -0,0 +1,13 @@
+package com.martinsgms.pyxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyxisApplication.class, args);
+	}
+
+}
---
+++ pyxis/pyxis-java/src/test/java/com/martinsgms/pyxis/PyxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinsgms.pyxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
